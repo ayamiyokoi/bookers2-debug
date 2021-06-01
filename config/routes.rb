329100259c03282b0e8882a5da016fd082000817
AyamiYokoi <didufd63hff@gmail.com>
2021-06-01 22:56:32 +0000

    [Add]following

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about', as: 'homes_about'
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users, only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   
   resources :books do
     resource :favorites, only: [:create, :destroy]
