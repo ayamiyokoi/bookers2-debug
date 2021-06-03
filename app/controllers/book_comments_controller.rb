195f380b57566f2d062609ba6c6909fdf63d46c4
AyamiYokoi <didufd63hff@gmail.com>
2021-06-03 01:35:29 +0000

    [Add]非同期通信コメント

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,19 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @book_comment = BookComment.new
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    render :comment
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+  	@book_comment = @book.book_comments.find(params[:id])
+		@book_comment.destroy
+    render :comment
   end
 
     private
